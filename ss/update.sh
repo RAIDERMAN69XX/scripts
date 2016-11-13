5f04e75b44c4356b1c0e62447aa00d93431bd179
Gerald <gera2ld@163.com>
2016-11-13 23:06:47 +0800

    fix pac update script

--- ss/update.sh
+++ ss/update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 CWD=`dirname $0`
+PROXY='SOCKS5 127.0.0.1:1080'
 
-genpac --gfwlist-url=https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt --user-rule-from=$CWD/user-rule.txt -p "SOCKS5 127.0.0.1:1080" -o $CWD/gfwlist.js
+genpac --gfwlist-url=https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt --user-rule-from=$CWD/user-rule.txt -p "$PROXY" --gfwlist-proxy="$PROXY" -o $CWD/gfwlist.js
